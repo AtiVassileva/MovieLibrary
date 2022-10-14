7f496acb0c0a24544c4da2a6e6fe85a457a8e778
Ati <ativassileva@gmail.com>
2022-10-14 12:19:40 +0300

    Refactored movie list view

--- src/MovieLibrary.Web/Controllers/MoviesController.cs
+++ src/MovieLibrary.Web/Controllers/MoviesController.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MovieLibrary.Data;
 using MovieLibrary.Models;
@@ -44,10 +39,7 @@ namespace MovieLibrary.Web.Controllers
         }
 
         // GET: Movies/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
+        public IActionResult Create() => View();
 
         // POST: Movies/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
