086d7af99e44e7c593bb41bab90bfcc25553af20
Ati <ativassileva@gmail.com>
2022-10-16 13:02:40 +0300

    Refactored home page

--- src/MovieLibrary.Web/Controllers/HomeController.cs
+++ src/MovieLibrary.Web/Controllers/HomeController.cs
@@ -1,21 +1,40 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MovieLibrary.Web.Models;
 using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using MovieLibrary.Data;
+using MovieLibrary.Web.Models.Movies;
 
 namespace MovieLibrary.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _context;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ApplicationDbContext context, ILogger<HomeController> logger)
         {
+            _context = context;
             _logger = logger;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var latestMovies = _context.Movies
+                .Take(3)
+                .Select(m => new MovieHomeModel
+                {
+                    Id = m.Id,
+                    Title = m.Title,
+                    Overview = m.Overview,
+                    ImageUrl = m.ImageUrl,
+                    GenreName =  _context.Genres
+                        .Where(g => g.Id == m.GenreId)
+                        .Select(g => g.Name)
+                        .FirstOrDefault()
+                });
+
+            return View(latestMovies);
         }
 
         public IActionResult Privacy()
---
+++ src/MovieLibrary.Web/Models/Movies/MovieHomeModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MovieLibrary.Web.Models.Movies
+{
+    public class MovieHomeModel
+    {
+        public Guid Id { get; set; }
+        public string? ImageUrl { get; set; }
+        public string? Title { get; set; }
+        public string? Overview { get; set; }
+        public string? GenreName { get; set; }
+    }
+}
