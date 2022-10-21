7a5c5783a6d46ae2885763efe3700809ecf24457
Ati <ativassileva@gmail.com>
2022-10-21 11:16:40 +0300

    Refactored home page

--- src/MovieLibrary.Web/Infrastructure/ClaimsPrincipalExtensions.cs
+++ src/MovieLibrary.Web/Infrastructure/ClaimsPrincipalExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
+using MovieLibrary.Models;
 
 namespace MovieLibrary.Web.Infrastructure
 {
@@ -6,5 +8,9 @@ namespace MovieLibrary.Web.Infrastructure
     {
         public static string? GetId(this ClaimsPrincipal user)
             => user.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+        public static bool HasLikedAMovie(this ClaimsPrincipal user, Guid? movieId, 
+            UserManager<User> manager)
+            => manager.GetUserAsync(user).Result.LikedMovies.Any(m => m.Id == movieId);
     }
 }
